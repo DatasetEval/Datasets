5ac31dc3a1e119599828895eb2768532eac188b1
Admin <raphael.1.dion@externe.bnpparibas.com>
2026-07-01 08:24:57 +0200

    Add PT0199_CyberSecEval4_prompt_injection_EN

---
+++ scripts/PT0199_CyberSecEval4_prompt_injection_EN.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_json('source_datasets/prompt_injection.json')
+
+df["prompt"] = df["user_input"]
+df["system_prompt"] = df["test_case_prompt"]
+df["category"] = "PT0199 Textual BOundary Mimicry"
+df["subcategory"] = df["injection_variant"]
+
+df_f = df[["prompt", "system_prompt", "category", "subcategory"]]
+df_f.to_csv("formated_datasets/PT0199_CyberSecEval4_prompt_injection_EN.csv")
+print(df_f.shape)
